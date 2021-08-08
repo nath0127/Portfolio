43c79e1b70e3563122db29904a31baad9da404af
Nathaniel Fung <nath0127@gmail.com>
2021-08-08 15:19:55 -0700

    finished posts CRUD graphql operations

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,35 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
 import express from 'express';
+import {ApolloServer} from 'apollo-server-express';
+import {buildSchema} from 'type-graphql';
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   orm.getMigrator().up();
 
   const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em })
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+
   app.listen(4000, () => {
     console.log('server started on localhost:4000')
   })
-
   // const posts = await orm.em.find(Post, {});
   // console.log(posts);
 };
